1148210fb34c9e8829272da8922b2e74ac78118e
dreamer <christopherlu.pdx@gmail.com>
2019-08-22 17:19:26 -0700

    failing my test

--- test/file1.test.js
+++ test/file1.test.js
@@ -1,16 +1,18 @@
 // import sortUrl from '../src/sortUrl';
 
 function sortUrl(array) {
+    console.log(array);
+    console.log(array.length);
     return array.length;
 }
 
 const test = QUnit.test;
 
 QUnit.module('day one');
-test('sortbylength can consume array and ', assert => {
+test('sortUrl knows how long arrays are', assert => {
     const array = ['we', 'all', 'succumb'];
-    // const expected = ['we', 'all', 'succumb'];
     const expected = 3;
+
     const sorted = sortUrl(array);
 
     assert.htmlEqual(sorted, expected);
