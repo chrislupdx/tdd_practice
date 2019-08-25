221462f2c041638a08d640d7e5636a4eedd23156
dreamer <christopherlu.pdx@gmail.com>
2019-08-24 20:22:06 -0700

    this is terrible

--- test/file1.test.js
+++ test/file1.test.js
@@ -5,30 +5,49 @@ function sortUrl(array) {
     //move through the array and compare the value of string to array[position of next]
     const newArray = [];
     array.forEach(string => {
+        function nextLength(nextNumber) {
+            if(array[nextNumber].length == undefined) {
+                console.log()
+                return 
+            }
+
+        }
         const nextNumber = array.indexOf(string) + 1;
         const nextLength = array[nextNumber].length;
 
-        // eslint-disable-next-line eqeqeq
         if(string.length > nextLength) {
             newArray.push(string);
+            console.log('greater', string);
+            console.log(array.indexOf(string), 'array pos');
         }
         if(string.length < nextLength) {
             newArray.unshift(string);
+            console.log('less', string);
+            console.log(array.indexOf(string), 'array pos');
         }
         // eslint-disable-next-line eqeqeq
-        if(array.lastIndexOf(string) == 0) {
-            console.log('last item is', string);
+        if(array.indexOf(string) == array.length - 2) {
+            console.log('last', string);
+            console.log(array.indexOf(string), 'array pos');
         }
-        console.log(array.length, 'newarray');
         return newArray;
     });
 }
+//treesorting is beautiful and feels performant but I can't figure out how to make this tree touch the last item
+//i'm really tempted to create an object with length, word, and index and sort by length but i'm convinced I can do this via tree generation
 
 
 QUnit.module('day one');
 
 test('sortUrl returns a list of re-arranged strings by length', assert => {
     const array = ['this', 'cheese', 'is', 'terrible'];
+    const arranged = sortUrl(array);
+    const expected = ['is', 'this', 'cheese', 'terrible'];
+
+    assert.equal(arranged, expected);
+});
+test('different word length', assert => {
+    const array = ['we', 'eart'];
     const arranged = sortUrl(array);
     const expected = ['is', 'this', 'cheese', 'terrible'];
 
